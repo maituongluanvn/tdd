f46c11cfed81e3f8da9b4fe30de06c2ce62129f1
luan_mai <luan.mai@ubitec.com>
2022-03-15 14:32:06 +0700

    update logic & unit test

--- src/calculator.ts
+++ src/calculator.ts
@@ -5,10 +5,41 @@ const add = (numbers?: string): number => {
   if (numbers.length === 1) {
     return Number(numbers);
   }
+
   let sum: number = 0;
-  const parts = numbers.split(',');
+  // check new line symbol & special characters and replace
+  let transformedNumbers: string = numbers;
+  const specialChars = /[`!@#$%^&*()_+\=\[\]{};':"\\|.<>\/?~]/gi;
+
+  if (numbers.includes('\n')) {
+    transformedNumbers = numbers.replace(/\n/gi, ',');
+  }
+  if (specialChars.test(numbers)) {
+    transformedNumbers = numbers.replace(specialChars, ',');
+  }
+
+  const parts = transformedNumbers.split(',');
+  let countNegativeNumbers: number[] = [];
+
   for (const part of parts) {
-    sum += Number(part);
+    const num = Number(part);
+    if (num < 0) {
+      countNegativeNumbers.push(num);
+    }
+    if (num <= 1000) {
+      sum += num;
+    }
+  }
+
+  // throw error when Number includes negative numbers
+  if (countNegativeNumbers.length === 1) {
+    throw new Error('negatives not allowed');
+  } else if (countNegativeNumbers.length > 1) {
+    let str = '';
+    for (const countNegativeNumber of countNegativeNumbers) {
+      str += ` ${countNegativeNumber}`;
+    }
+    throw new Error(`negatives not allowed${str}`);
   }
   return sum;
 };
--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -3,6 +3,13 @@ import assert from 'assert';
 
 describe('calculator', () => {
   describe('add function', () => {
+    test('validate arguments', () => {
+      expect(add('abc')).toBe(0);
+    });
+    test('should return NaN when arguments is one characters and its not number', () => {
+      expect(add(',')).toBeNaN();
+      expect(add('w')).toBeNaN();
+    });
     test('should return 0 when arguments is empty', () => {
       expect(add()).toBe(0);
     });
@@ -18,8 +25,21 @@ describe('calculator', () => {
       expect(add('6,9')).toBe(15);
     });
     test('2. Allow the Add method to handle an unknown amount of numbers', () => {
-      expect(add('1,2,123,145,4567')).toBe(4838);
-      expect(add('-1,2,123,145,4567')).toBe(4836);
+      expect(add('1,2,123,145')).toBe(271);
+    });
+    test('3. Allow the Add method to handle new lines between numbers', () => {
+      expect(add('1\n2,3')).toBe(6);
+      expect(add('1\n2\n3')).toBe(6);
+    });
+    test('5. Calling Add with a negative number will throw an exception', () => {
+      expect(() => add('-1, -3')).toThrow('negatives not allowed -1 -3');
+      expect(() => add('-1, 3')).toThrow('negatives not allowed');
+    });
+    test('6. Numbers bigger than 1000 should be ignored', () => {
+      expect(add('100000, 2, 3, 20000, 1000')).toEqual(1005);
+    });
+    test('7. Delimiters can be of any length with the following format', () => {
+      expect(add('//[***]\n1***2***3**&(*&(*&4')).toEqual(10);
     });
   });
 });
