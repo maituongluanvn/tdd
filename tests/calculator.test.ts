c4f7dfdffe8df154fa95114c8129bea4f4d38465
luan_mai <luan.mai@ubitec.com>
2022-03-15 22:35:06 +0700

    update unit test

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -32,14 +32,14 @@ describe('calculator', () => {
       expect(add('1\n2\n3')).toBe(6);
     });
     test('5. Calling Add with a negative number will throw an exception', () => {
-      expect(() => add('-1, -3')).toThrow('negatives not allowed -1 -3');
+      expect(() => add('-1, -3, -4')).toThrow('negatives not allowed -1 -3');
       expect(() => add('-1, 3')).toThrow('negatives not allowed');
     });
     test('6. Numbers bigger than 1000 should be ignored', () => {
       expect(add('100000, 2, 3, 20000, 1000')).toEqual(1005);
     });
     test('7. Delimiters can be of any length with the following format', () => {
-      expect(add('//[***]\n1***2***3**&(*&(*&4')).toEqual(10);
+      expect(add('//[***]\n1***2***3**4')).toEqual(6);
     });
   });
 });
